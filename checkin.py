a1e92bc9a3805fc8841ccb3cff9688b529dcbf26
dbqwcx123 <84033827+dbqwcx123@users.noreply.github.com>
2024-11-28 15:01:04 +0800

    Update checkin.py
    back

--- checkin.py
+++ checkin.py
@@ -45,8 +45,6 @@ if __name__ == '__main__':
             # 获取账号当前状态
             result = state.json()
             print(result.keys())
-            print(result['code'])
-            print(result['message'])
             # 获取剩余时间
             leftdays = int(float(result['data']['leftDays']))
             # 获取账号email
